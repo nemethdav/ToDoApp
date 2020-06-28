50903feef0f486e3cff9bcd38d45b1c012e3975b
nemethdav <60889598+nemethdav@users.noreply.github.com>
2020-06-28 17:00:20 +0200

    2. ToDo migrációs, controller, model fájlok létrehozása, migrációs fájl elkészítése

---
+++ database/migrations/2020_06_28_144331_create_to_dos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateToDosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('to_dos', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->dateTime('deadline');
+            $table->boolean('completed')->default(false);
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('to_dos');
+    }
+}
