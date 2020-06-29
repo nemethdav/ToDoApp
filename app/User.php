c25f972304c7df56d6e8d0abd83ab8cf262dba32
nemethdav <60889598+nemethdav@users.noreply.github.com>
2020-06-29 20:06:29 +0200

    Model-ek összekötése

--- app/User.php
+++ app/User.php
@@ -36,4 +36,9 @@ class User extends \TCG\Voyager\Models\User
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function todos()
+    {
+        return $this->hasMany(ToDo::class);
+    }
 }
